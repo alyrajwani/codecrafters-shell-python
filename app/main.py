f2d9c3db77fd7b53d309ac373f89d3aec91ccbbe
alyrajwani <aly_rajwani@brown.edu>
2025-01-04 16:55:09 -0800

    type

--- app/main.py
+++ app/main.py
@@ -1,22 +1,46 @@
 import sys
 
+VALID_COMMANDS = ["exit", "echo", "type"]
+
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_input = input()
-        if len(user_input.split(" ", 1)) == 1:
-            [command] = user_input.split(" ", 1)
-        else:
-            [command, args] = user_input.split(" ", 1)
-
+        command = user_input.split(" ", 1)[0]
+        args = None
+        if len(user_input.split(" ", 1)) == 2:
+            args = user_input.split(" ", 1)[1]
         match command:
             case "exit": 
-                break
+                handle_exit(command, args)
             case "echo":
-                print(f"{args}")
+                handle_echo(command, args)
+            case "type":
+                handle_type(command, args)
             case _:
                 print(f"{command}: command not found")
+
+def handle_echo(command, args):
+    if not args:
+        continue
+    else:
+        print(f"{args}")
+
+def handle_exit(command, args):
+    if not args:
+        continue
+    else:
+        exit(int(args))
+
+def handle_type(command, args):
+    if not args:
+        continue
+    else:
+        if args in VALID_COMMANDS:
+            print(f"{args} is a shell builtin")
+        else:
+            print(f"{args}: not found")
 
 if __name__ == "__main__":
     main()
