b417729c05fadcf1c679e1f50e2550ab99eb2ac1
alyrajwani <aly_rajwani@brown.edu>
2025-01-04 16:18:34 -0800

    REPL

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import sys
 
 def main():
-    sys.stdout.write("$ ")
-
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found\n")
+    valid_commands = []
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
+        if command not in valid_commands:
+            print(f"${command}: command not found")
+            continue
 
 if __name__ == "__main__":
     main()
