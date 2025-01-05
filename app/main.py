c068999be6eebda032970cbcd30200f5789f097c
alyrajwani <aly_rajwani@brown.edu>
2025-01-04 16:11:48 -0800

    handle invalid

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 import sys
 
-
 def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
-
+    command = input()
+    print(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
