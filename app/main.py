28518a23e9e4c0be216ae52a95e4297203d09b9c
alyrajwani <aly_rajwani@brown.edu>
2025-01-04 16:34:19 -0800

    echo

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
 
         match command:
             case "exit": 
-                sys.exit(args)
+                break
             case "echo":
                 print(f"{args}")
             case _:
