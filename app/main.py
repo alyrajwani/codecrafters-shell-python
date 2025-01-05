6ba91018505509ab9c974cfafe20093c23baae0e
alyrajwani <aly_rajwani@brown.edu>
2025-01-05 00:17:53 -0800

    run a program

--- app/main.py
+++ app/main.py
@@ -1,58 +1,49 @@
 import os
+import subprocess
 import sys
 
-VALID_COMMANDS = ["exit", "echo", "type"]
+def locate_executable(command):
+    paths = os.getenv("PATH").split(":")
+    for path in paths:
+        file_path = os.path.join(path, command)
+        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+            return file_path
+    return None
+
+def handle_exit(args):
+    sys.exit(int(args[0]) if args else 0)
+
+def handle_echo(args):
+    print(" ".join(args))
+
+def handle_type(args):
+    if args[0] in VALID_COMMAND_DICT:
+        print(f"{args[0]} is a shell builtin")
+    elif executable := locate_executable(args[0]):
+        print(f"{args[0]} is {executable}")
+    else:
+        print(f"{args[0]} not found")
+
+VALID_COMMAND_DICT = {
+    "exit": handle_exit,
+    "echo": handle_echo,
+    "type": handle_type,
+}
 
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        user_input = input()
-        command = user_input.split(" ", 1)[0]
-        args = None
-        if len(user_input.split(" ", 1)) == 2:
-            args = user_input.split(" ", 1)[1]
-        paths = os.getenv("PATH").split(":")
-
-        match command:
-            case "exit": 
-                handle_exit(command, args)
-            case "echo":
-                handle_echo(command, args)
-            case "type":
-                handle_type(command, args, paths)
-            case _:
-                if os.path.isfile(command):
-                    os.system(command)
-                else:
-                    print(f"{command}: command not found")
-
-def handle_echo(command, args):
-    if not args:
-        pass
-    else:
-        print(f"{args}")
-
-def handle_exit(command, args):
-    if not args:
-        pass
-    else:
-        exit(int(args))
-
-def handle_type(command, args, paths):
-    if not args:
-        pass
-    elif args in VALID_COMMANDS:
-        print(f"{args} is a shell builtin")
-    else: 
-        arg_path = None
-        for path in paths:
-            if os.path.isfile(f"{path}/{args}"):
-                arg_path = f"{path}/{args}"
-        if arg_path:
-            print(f"{args} is {arg_path}")
+        
+        command, *args = input().split(" ")
+        if command in VALID_COMMAND_DICT:
+            builtins[command](args)
+            continue
+        elif executable := locate_executable(command):
+            subprocess.run([executable, *args])
         else:
-            print(f"{args}: not found")
+            print(f"{command}: command not found")
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
